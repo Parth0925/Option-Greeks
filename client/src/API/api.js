cd0447671b9f3aa06f9856a18f74888f186fe0e9
Parth0925 <parth.soni0925@gmail.com>
2025-05-14 13:55:06 +0530

    build-error

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -1,6 +1,5 @@
 // src/API/api.js
 import axios from 'axios';
-require('dotenv').config();
 
 //const BASE_URL = 'http://localhost:5001/api';
 //const BASE_URL = "https://option-greeks-iygo.onrender.com"
